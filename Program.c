b5a145ad39fc5e407e5f3c328a8acb69e2bb1280
mmmello <matheusmello142012@gmail.com>
2022-02-18 10:09:13 -0300

    Open txt in C

---
+++ Program.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    FILE *archive;
+    char content[1000] = "", content_archive[100];
+
+    system("cls");
+
+    archive = fopen("test.txt", "r");
+
+    if(archive == NULL){
+        printf("\nFile opening failed.\n");
+
+        fclose(archive);
+        exit(0);
+    }else{
+        while(fgets(content_archive, 100, archive))
+            strcat(content, content_archive);
+        
+        printf(content);
+    }
+
+    fclose(archive);
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Open txt file in C language
+
+This is a simple program designed to instruct any user to simply open a txt file in the C programming language.
+
+For the program to work, the file test.txt must be in the same directory as program.c. If you want to read a file from another location, you have to change the path in the fopen function.
